d7e694432ec4df5e431e1c71384c61a6e2c34111
Cherry <87404668+aspiringLilCoder@users.noreply.github.com>
2024-11-09 03:53:07 +0900

    Create DelegateVotingPowerToSelf.ts

---
+++ scripts/cherry/DelegateVotingPowerToSelf.ts
@@ -0,0 +1,54 @@
+import {
+  createPublicClient,
+  http,
+  createWalletClient,
+  hexToString,
+  parseEther,
+} from "viem";
+import { privateKeyToAccount } from "viem/accounts";
+import { sepolia } from "viem/chains";
+import { abi } from "../../artifacts/contracts/MyERC20Votes.sol/MyToken.json";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const providerApiKey = process.env.ALCHEMY_API_KEY || "";
+const deployerPrivateKey = process.env.PRIVATE_KEY || "";
+
+async function main() {
+  // Contract address
+  const contractAddress = "0x85d57140a0db24d45ee2bb3379e548d1253bfa36";
+  if (!contractAddress) throw new Error("Contract address not provided");
+  if (!/^0x[a-fA-F0-9]{40}$/.test(contractAddress))
+    throw new Error("Invalid contract address");
+
+  // Create public cient and wallet client
+  const publicClient = createPublicClient({
+    chain: sepolia,
+    transport: http(providerApiKey),
+  });
+
+  const account = privateKeyToAccount(`0x${deployerPrivateKey}`);
+  const deployer = createWalletClient({
+    account,
+    chain: sepolia,
+    transport: http(`https://eth-sepolia.g.alchemy.com/v2/${providerApiKey}`),
+  });
+
+  // Delegate to self
+  const delegateTx = await deployer.writeContract({
+    address: contractAddress,
+    abi: abi,
+    functionName: "delegate",
+    args: [deployer.account.address],
+  });
+
+  console.log("Transaction hash:", delegateTx);
+  console.log("Waiting for confirmations...");
+  await publicClient.waitForTransactionReceipt({ hash: delegateTx });
+  console.log("Transaction confirmed");
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
