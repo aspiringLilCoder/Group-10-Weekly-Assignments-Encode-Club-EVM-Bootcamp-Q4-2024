b8135212c16e16af92ab1c8aa03c54376349111b
Cherry <87404668+aspiringLilCoder@users.noreply.github.com>
2024-11-25 14:37:56 +0900

    Deploy TokenizedBallot

--- frontend/scaffold-eth-2/packages/nextjs/contracts/deployedContracts.ts
+++ frontend/scaffold-eth-2/packages/nextjs/contracts/deployedContracts.ts
@@ -4,168 +4,329 @@
  */
 import { GenericContractsDeclaration } from "~~/utils/scaffold-eth/contract";
 
-const deployedContracts = {} as const;
+const deployedContracts = {
+  31337: {
+    TokenizedBallot: {
+      address: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+      abi: [
+        {
+          inputs: [
+            {
+              internalType: "bytes32[]",
+              name: "_proposalNames",
+              type: "bytes32[]",
+            },
+            {
+              internalType: "address",
+              name: "_tokenContract",
+              type: "address",
+            },
+            {
+              internalType: "uint256",
+              name: "_targetBlockNumber",
+              type: "uint256",
+            },
+          ],
+          stateMutability: "nonpayable",
+          type: "constructor",
+        },
+        {
+          inputs: [
+            {
+              internalType: "address",
+              name: "voter",
+              type: "address",
+            },
+          ],
+          name: "getVotePower",
+          outputs: [
+            {
+              internalType: "uint256",
+              name: "",
+              type: "uint256",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+        {
+          inputs: [
+            {
+              internalType: "uint256",
+              name: "",
+              type: "uint256",
+            },
+          ],
+          name: "proposals",
+          outputs: [
+            {
+              internalType: "bytes32",
+              name: "name",
+              type: "bytes32",
+            },
+            {
+              internalType: "uint256",
+              name: "voteCount",
+              type: "uint256",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+        {
+          inputs: [],
+          name: "targetBlockNumber",
+          outputs: [
+            {
+              internalType: "uint256",
+              name: "",
+              type: "uint256",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+        {
+          inputs: [],
+          name: "tokenContract",
+          outputs: [
+            {
+              internalType: "contract IMyToken",
+              name: "",
+              type: "address",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+        {
+          inputs: [
+            {
+              internalType: "uint256",
+              name: "proposal",
+              type: "uint256",
+            },
+            {
+              internalType: "uint256",
+              name: "amount",
+              type: "uint256",
+            },
+          ],
+          name: "vote",
+          outputs: [],
+          stateMutability: "nonpayable",
+          type: "function",
+        },
+        {
+          inputs: [
+            {
+              internalType: "address",
+              name: "",
+              type: "address",
+            },
+          ],
+          name: "votePowerSpent",
+          outputs: [
+            {
+              internalType: "uint256",
+              name: "",
+              type: "uint256",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+        {
+          inputs: [],
+          name: "winnerName",
+          outputs: [
+            {
+              internalType: "bytes32",
+              name: "winnerName_",
+              type: "bytes32",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+        {
+          inputs: [],
+          name: "winningProposal",
+          outputs: [
+            {
+              internalType: "uint256",
+              name: "winningProposal_",
+              type: "uint256",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+      ],
+      inheritedFunctions: {},
+    },
+  },
+  11155111: {
+    TokenizedBallot: {
+      address: "0xCFF30Ee654b977297d78F4Be1AAbb77A1CB7B913",
+      abi: [
+        {
+          inputs: [
+            {
+              internalType: "bytes32[]",
+              name: "_proposalNames",
+              type: "bytes32[]",
+            },
+            {
+              internalType: "address",
+              name: "_tokenContract",
+              type: "address",
+            },
+            {
+              internalType: "uint256",
+              name: "_targetBlockNumber",
+              type: "uint256",
+            },
+          ],
+          stateMutability: "nonpayable",
+          type: "constructor",
+        },
+        {
+          inputs: [
+            {
+              internalType: "address",
+              name: "voter",
+              type: "address",
+            },
+          ],
+          name: "getVotePower",
+          outputs: [
+            {
+              internalType: "uint256",
+              name: "",
+              type: "uint256",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+        {
+          inputs: [
+            {
+              internalType: "uint256",
+              name: "",
+              type: "uint256",
+            },
+          ],
+          name: "proposals",
+          outputs: [
+            {
+              internalType: "bytes32",
+              name: "name",
+              type: "bytes32",
+            },
+            {
+              internalType: "uint256",
+              name: "voteCount",
+              type: "uint256",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+        {
+          inputs: [],
+          name: "targetBlockNumber",
+          outputs: [
+            {
+              internalType: "uint256",
+              name: "",
+              type: "uint256",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+        {
+          inputs: [],
+          name: "tokenContract",
+          outputs: [
+            {
+              internalType: "contract IMyToken",
+              name: "",
+              type: "address",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+        {
+          inputs: [
+            {
+              internalType: "uint256",
+              name: "proposal",
+              type: "uint256",
+            },
+            {
+              internalType: "uint256",
+              name: "amount",
+              type: "uint256",
+            },
+          ],
+          name: "vote",
+          outputs: [],
+          stateMutability: "nonpayable",
+          type: "function",
+        },
+        {
+          inputs: [
+            {
+              internalType: "address",
+              name: "",
+              type: "address",
+            },
+          ],
+          name: "votePowerSpent",
+          outputs: [
+            {
+              internalType: "uint256",
+              name: "",
+              type: "uint256",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+        {
+          inputs: [],
+          name: "winnerName",
+          outputs: [
+            {
+              internalType: "bytes32",
+              name: "winnerName_",
+              type: "bytes32",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+        {
+          inputs: [],
+          name: "winningProposal",
+          outputs: [
+            {
+              internalType: "uint256",
+              name: "winningProposal_",
+              type: "uint256",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+      ],
+      inheritedFunctions: {},
+    },
+  },
+} as const;
 
 export default deployedContracts satisfies GenericContractsDeclaration;
-
-// 11155111: {
-//   TokenizedBallot: {
-//     address: "0x9d7b89C9Efa2ED6ba2130164A0FB39A1978aB925",
-//     abi: [
-//       {
-//         inputs: [
-//           {
-//             internalType: "bytes32[]",
-//             name: "_proposalNames",
-//             type: "bytes32[]",
-//           },
-//           {
-//             internalType: "address",
-//             name: "_tokenContract",
-//             type: "address",
-//           },
-//           {
-//             internalType: "uint256",
-//             name: "_targetBlockNumber",
-//             type: "uint256",
-//           },
-//         ],
-//         stateMutability: "nonpayable",
-//         type: "constructor",
-//       },
-//       {
-//         inputs: [
-//           {
-//             internalType: "address",
-//             name: "voter",
-//             type: "address",
-//           },
-//         ],
-//         name: "getVotePower",
-//         outputs: [
-//           {
-//             internalType: "uint256",
-//             name: "",
-//             type: "uint256",
-//           },
-//         ],
-//         stateMutability: "view",
-//         type: "function",
-//       },
-//       {
-//         inputs: [
-//           {
-//             internalType: "uint256",
-//             name: "",
-//             type: "uint256",
-//           },
-//         ],
-//         name: "proposals",
-//         outputs: [
-//           {
-//             internalType: "bytes32",
-//             name: "name",
-//             type: "bytes32",
-//           },
-//           {
-//             internalType: "uint256",
-//             name: "voteCount",
-//             type: "uint256",
-//           },
-//         ],
-//         stateMutability: "view",
-//         type: "function",
-//       },
-//       {
-//         inputs: [],
-//         name: "targetBlockNumber",
-//         outputs: [
-//           {
-//             internalType: "uint256",
-//             name: "",
-//             type: "uint256",
-//           },
-//         ],
-//         stateMutability: "view",
-//         type: "function",
-//       },
-//       {
-//         inputs: [],
-//         name: "tokenContract",
-//         outputs: [
-//           {
-//             internalType: "contract IMyToken",
-//             name: "",
-//             type: "address",
-//           },
-//         ],
-//         stateMutability: "view",
-//         type: "function",
-//       },
-//       {
-//         inputs: [
-//           {
-//             internalType: "uint256",
-//             name: "proposal",
-//             type: "uint256",
-//           },
-//           {
-//             internalType: "uint256",
-//             name: "amount",
-//             type: "uint256",
-//           },
-//         ],
-//         name: "vote",
-//         outputs: [],
-//         stateMutability: "nonpayable",
-//         type: "function",
-//       },
-//       {
-//         inputs: [
-//           {
-//             internalType: "address",
-//             name: "",
-//             type: "address",
-//           },
-//         ],
-//         name: "votePowerSpent",
-//         outputs: [
-//           {
-//             internalType: "uint256",
-//             name: "",
-//             type: "uint256",
-//           },
-//         ],
-//         stateMutability: "view",
-//         type: "function",
-//       },
-//       {
-//         inputs: [],
-//         name: "winnerName",
-//         outputs: [
-//           {
-//             internalType: "bytes32",
-//             name: "winnerName_",
-//             type: "bytes32",
-//           },
-//         ],
-//         stateMutability: "view",
-//         type: "function",
-//       },
-//       {
-//         inputs: [],
-//         name: "winningProposal",
-//         outputs: [
-//           {
-//             internalType: "uint256",
-//             name: "winningProposal_",
-//             type: "uint256",
-//           },
-//         ],
-//         stateMutability: "view",
-//         type: "function",
-//       },
-//     ],
-//     inheritedFunctions: {},
-//   },
-// },
